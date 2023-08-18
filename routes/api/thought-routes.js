98511d8ffad5d47e8346dc5dc4fd0cf31bc5bb58
Joshua Hebert <jhebertcocktails@gmail.com>
2023-08-18 14:18:20 -0400

    thought-routes.js created

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const {
+    getThoughts,
+    getThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction,
+} = require('../../controllers/thought-controller');
+
+// API Routes for /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// API Routes for /api/thoughts/:id
+router
+    .route('/:id')
+    .get(getThought)
+    .put(updateThought)
+    .delete(deleteThought);
+
+// API Routes for /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction)
+
+// API Routes for /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction)
+
+module.exports = router;
