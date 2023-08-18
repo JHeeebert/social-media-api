797ebfbe5c3353ef8d11ce1aba4a38c70b31f123
Joshua Hebert <jhebertcocktails@gmail.com>
2023-08-18 15:10:41 -0400

    added notes and fixed DB name

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,8 @@
 const { connect, connection } = require('mongoose');
+// Creates the DB
 const connectionString =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
-
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialMediaDB';
+// Connection between Mongoose and MongoDB
 connect(connectionString);
 
 module.exports = connection;
