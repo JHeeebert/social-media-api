011d4ec380446d7445e61a97d1aec35170ec0e1b
Joshua Hebert <jhebertcocktails@gmail.com>
2023-08-18 13:28:16 -0400

    added code to index.js in routes folder

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const port = process.env.PORT || 3001;
 const app = express();
 
 // An extra funstion to help indicate what activity's server is running in the terminal.
-// const activity = cwd.includes('01-Activities')
-// ? cwd.split('01-Activities')[1]
-// : cwd;
+const activity = cwd.includes('01-Activities')
+? cwd.split('01-Activities')[1]
+: cwd;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
