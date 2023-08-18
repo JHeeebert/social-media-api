1bd5547053bd74090fd9412d840ab2af3085bf45
Joshua Hebert <jhebertcocktails@gmail.com>
2023-08-18 13:36:28 -0400

    added notes to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-
+// Middelware
 router.use("/api", apiRoutes)
 
 router.use((req, res) => {
     return res.send("Turn Around, Wrong Route!")
 })
-
+// Exports router
 module.exports = router;
