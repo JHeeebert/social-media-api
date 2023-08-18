645e377fcb99abae935bac3ab868d5d47108d5e7
Joshua Hebert <jhebertcocktails@gmail.com>
2023-08-18 13:40:01 -0400

    more concise notes

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
-
+// Sets the PORT for the server to listen on
 const port = process.env.PORT || 3001;
+// Create Express app
 const app = express();
 
 // Middleware
