5e3e791e82de4c870d619590bd2305b5abadd646
Joshua Hebert <jhebertcocktails@gmail.com>
2023-08-18 13:38:47 -0400

    removed cwd and added notes

--- server.js
+++ server.js
@@ -2,22 +2,17 @@ const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const cwd = process.cwd();
-
 const port = process.env.PORT || 3001;
 const app = express();
 
-// An extra funstion to help indicate what activity's server is running in the terminal.
-const activity = cwd.includes('01-Activities')
-? cwd.split('01-Activities')[1]
-: cwd;
-
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes());
-
+// Connection callback for DB connection
 db.once('open', () => {
+    // Server will listen on specified port
     app.listen(PORT, () => {
-        console.log(`Server is running on port ${port} in ${activity}`);
+        console.log(`Server is running on port ${port}!`);
     });
 });
