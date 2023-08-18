16c5db04cf57b04213fe3d98ff83d233f4f8e78d
Joshua Hebert <jhebertcocktails@gmail.com>
2023-08-18 13:15:52 -0400

    added code to server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const cwd = process.cwd();
+
+const port = process.env.PORT || 3001;
+const app = express();
+
+// An extra funstion to help indicate what activity's server is running in the terminal.
+// const activity = cwd.includes('01-Activities')
+// ? cwd.split('01-Activities')[1]
+// : cwd;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes());
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${port} in ${activity}`);
+    });
+});
