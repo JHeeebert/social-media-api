1d31d7c34e210253ec4cac765177f6c80ad1803b
Joshua Hebert <jhebertcocktails@gmail.com>
2023-08-19 15:03:54 -0400

    added notes

--- config/connection.js
+++ config/connection.js
@@ -4,5 +4,5 @@ const connectionString =
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialMediaDB';
 // Connection between Mongoose and MongoDB
 connect(connectionString);
-
+//  Export the connection
 module.exports = connection;
