1c94112148fb39ee5fd56bedad3a25399686fe6e
Joshua Hebert <jhebertcocktails@gmail.com>
2023-08-19 15:01:14 -0400

    fixed error handling and added notes

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,7 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 // Middelware
 router.use("/api", apiRoutes)
-
-router.use((req, res) => {
-    return res.send("Turn Around, Wrong Route!")
-})
+// Error handling
+router.use((req, res) =>  res.send("Turn Around, Wrong Route!"));
 // Exports router
 module.exports = router;
